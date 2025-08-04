789ae27bc0a0db150877f8a4a709508be91cf824
lloyd730 <danillusto59@gmail.com>
2025-08-04 20:57:16 -0300

    Adiciona tipagem e listar decks

--- src/services/anki_deck_manager.py
+++ src/services/anki_deck_manager.py
@@ -1,11 +1,13 @@
 import requests
+from typing import List, TypedDict, Optional
+from models.cards import Card
 
 class AnkiDeckManager:
-    def __init__(self, deck_name):
-        self.deck_name = deck_name
-        self.anki_connect_url = "http://localhost:8765"
+    def __init__(self, deck_name: str) -> None:
+        self.deck_name: str = deck_name
+        self.anki_connect_url: str = "http://localhost:8765"
 
-    def add_cards(self, cards):
+    def add_cards(self, cards: List[Card]) -> bool:
         for card in cards:
             payload = {
                 "action": "addNote",
@@ -32,3 +34,20 @@ class AnkiDeckManager:
                 print("Erro: não conseguiu conectar no AnkiConnect.")
                 return False
         return True
+
+    def listar_decks(self) -> List[str]:
+        try:
+            import urllib.request, json
+            req = {
+                "action": "deckNames",
+                "version": 6
+            }
+            data = json.dumps(req).encode("utf-8")
+            response = urllib.request.urlopen(
+                urllib.request.Request(self.anki_connect_url, data=data)
+            )
+            decks = json.load(response)["result"]
+            return decks
+        except Exception as e:
+            print("Erro ao listar decks:", e)
+            return []
